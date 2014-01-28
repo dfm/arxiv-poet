57a6af5540576e33764494de6d86428cf9c127fa
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2014-01-28 17:07:54 +0000

    ignore references (.,)

--- haiku.py
+++ haiku.py
@@ -41,30 +41,76 @@ def find_haiku(st):
                         for word in word_list:
                             m += word[1]
                             if m == 12:
-                                return sentence
-    return 'No Haiku found'
+                                return build_sentence(sentence)
+    return " "
 
 # Find sentences with 5, 7 or 12 syllables
 def find_alt_forms(st):
-    nsyls = []
+    s5 = []; s7 = []; s12 = []
     for sentence in st:
         word_list = syl_count(sentence)
         if word_list != None:
             sylno = sum([w[1] for w in word_list])
-            if sylno == 5: print sylno
-            elif sylno == 7: print sylno
-            elif sylno == 12: print sylno
+            if sylno == 5:
+                bs = build_sentence(sentence)
+                if bs.count('.,') == 0:
+                    print "(5)", bs
+                    s5.append(bs)
+            elif sylno == 7:
+                bs = build_sentence(sentence)
+                if bs.count('.,') == 0:
+                    print "(7)", bs
+                    s7.append(bs)
+            elif sylno == 12:
+                bs = build_sentence(sentence)
+                if bs.count('.,') == 0:
+                    print "(12)", bs
+                    s12.append(bs)
+    return s5, s7, s12
 
+def build_haiku(s5, s7, s12):
+    if len(s5) > 1 and len(s7) > 0:
+        return s5[0] + "\n", s7[0] + "\n", s5[1]
+    if len(s5) > 0 and len(s12) > 0:
+        return s12[0] + "\n", s5[0]
+
+def build_sentence(words):
+    s = " "
+    for w in words:
+        if w == "$":
+            continue
+        if not len(w.strip(string.punctuation)):
+            if w in ["(", "{", "\{", "[", "`", "``"]:
+                s += w
+            else:
+                s = s[:-1]+w+" "
+        else:
+            s += w+" "
+    s = s.strip()
+    s = s[0].upper() + s[1:]
+    return s
 
 def load_paper(arxiv_id):
     return arxiv.get_article(arxiv_id)
 
-# 1202.3665 emcee paper
-# 1306.3701 Van saders
-# 1307.3239 Tom's paper
-# 1401.6128 Fengji's paper
-sk1 = "1309.0419"
+dfm1 = "1202.3665"
+dfm2 = "1310.4179"
+dfm3 = "1211.6105"
+dfm4 = "1211.5805"
+dfmra1 = "1309.0654"
+dfmra2 = "1309.0653"
+fh1 = "1401.6128" # haiku
+sk2 = "1207.2945"
+sk3 = "1203.5486"
+rs2 = "1106.1885"
+rs3 = "0807.4382"
+bs1 = "1211.0278" # haiku
+bp1 = "1302.6682"
+bp2 = "1305.1934"
 
-st = load_paper(sk1)
+# st = load_paper(bs1)
+# st = load_paper(fh1)
+st = load_paper(dfm3)
 print find_haiku(st)
-print find_alt_forms(st)
+s5, s7, s12 = find_alt_forms(st)
+print build_haiku(s5, s7,s12)
