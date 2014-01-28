3f240b21ab0f36636ab5d9d0a1ccb34b6bf91d99
Dan F-M <danfm@nyu.edu>
2014-01-28 16:17:46 +0000

    rhyme scores

--- nlp.py
+++ nlp.py
@@ -36,19 +36,26 @@ def distance(w1, w2):
     return sum([c1 == c2 for c1, c2 in izip(w1[:n], w2[:n])]) / n
 
 
-def score_rhyme(p1, p2):
+def compute(args):
+    s1, s2 = args
+    for i, (c1, c2) in enumerate(izip(s1[::-1], s2[::-1])):
+        if c1 != c2:
+            break
+    return min(4.0, i) / 4.0
+
+
+def score_rhyme(p1, p2, a=0.6):
     # Flatten the phoneme lists.
     s1 = [[w for s in p for w in s] for p in product(*p1)]
     s2 = [[w for s in p for w in s] for p in product(*p2)]
 
-    # Compute the suffix phoneme scores.
-    score, norm = 0.0, 0.0
-    for depth, v in izip(range(2, 6), [0.05, 0.15, 0.4, 0.4]):
-        for s01, s02 in product(s1, s2):
-            w1, w2 = "".join(s01[-depth:]), "".join(s02[-depth:])
-            score += v * distance(w1, w2)
-            norm += v
-    score /= norm
+    # Reject the sentence if any pronunciations are identical.
+    if any([t1 == t2 for t1, t2 in product(s1, s2)]):
+        return 0
+
+    # Compute the score for each combination.
+    scores = map(compute, product(s1, s2))
+    score = sum(scores) / len(scores)
 
     # Extract the vowels.
     v1 = [[w[:-1] for w in s if w[-1] in string.digits] for s in s1]
@@ -58,11 +65,10 @@ def score_rhyme(p1, p2):
                for i1, i2 in product(range(len(v1)), range(len(v1)))]
     vscore = sum([d * w for d, w in izip(dists, weights)]) / sum(weights)
 
-    a = 0.5
     return (1-a)*score + a*vscore
 
 
 if __name__ == "__main__":
     w, p1, s, c = pronunciation(["Dude", "in", "an", "underwater", "tin"])
-    w, p2, s, c = pronunciation(["Dude", "in", "an", "underwater", "tin"])
+    w, p2, s, c = pronunciation(["Dude", "in", "an", "underwater", "spin"])
     print(score_rhyme(p1, p2))
